c2d4a9cb9bace9aeb664d6b4d488d41cd580e1ee
Dylan Sadoun <dysad@l4714-22.info.polymtl.ca>
2015-09-11 15:24:28 -0400

    Premier Lab. Accueil, layout, 3 pages, un peu de JS

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res) {
+    // Cette fonction va tenter de convertir le fichier jade situé à
+    // view/index.jade en HTML pour l'envoyer vers le client.
+    // On profite pour envoyer au client l'attribut « title ».
+    res.render('index', { title: "Lone Wolf 2" });
+});
+
+router.get('/informationsPersonnage', function(req, res) {
+    res.render('informations_personnage');
+});
+
+router.get('/nouveauPersonnage', function(req, res) {
+    res.render('nouveau_personnage');
+});
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// http://stackoverflow.com/questions/20089582/how-to-get-url-parameter-in-express-node-js
+// http://stackoverflow.com/questions/12132978/use-a-variable-in-a-jade-include
+router.get('/page/:valeur', function(req, res, next) {
+  // On récupère le paramètre de l'URL
+  var v = req.params.valeur
+
+  // On crée dynamiquement la page qu'on souhaite charger
+  var page = "./page" + v + ".jade"
+
+  // On veut d'abord convertir la page en HTML, une fois que la conversion
+  // est faite, on va injecter le HTML généré vers le fichier page.jade
+  res.render(page, function(err, html) {
+      res.render('page', { title: v, htmlPage: html })
+  });
 });
 
 module.exports = router;

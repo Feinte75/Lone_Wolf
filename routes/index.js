d18532cc8b6c381cbb2b6a365c5e8bc03c928b9a
Dylan Sadoun <didjidan@hotmail.fr>
2015-09-16 15:43:40 -0400

    Suite Lab1. Ajouts page_de_jeu, aide, corrections diverses, creation personnage

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,19 @@ router.get('/', function(req, res) {
     // Cette fonction va tenter de convertir le fichier jade situé à
     // view/index.jade en HTML pour l'envoyer vers le client.
     // On profite pour envoyer au client l'attribut « title ».
-    res.render('index', { title: "Lone Wolf 2" });
-});
-
-router.get('/informationsPersonnage', function(req, res) {
-    res.render('informations_personnage');
+    res.render('index', { title: "Lone Wolf (title)" });
 });
 
 router.get('/nouveauPersonnage', function(req, res) {
     res.render('nouveau_personnage');
+});
+
+router.get('/pagedejeu', function(req, res) {
+    res.render('page_de_jeu');
+});
+
+router.get('/aide', function(req, res) {
+    res.render('aide');
 });
 
 
@@ -25,7 +29,7 @@ router.get('/page/:valeur', function(req, res, next) {
   var v = req.params.valeur
 
   // On crée dynamiquement la page qu'on souhaite charger
-  var page = "./page" + v + ".jade"
+  var page = "./pages/page" + v + ".jade"
 
   // On veut d'abord convertir la page en HTML, une fois que la conversion
   // est faite, on va injecter le HTML généré vers le fichier page.jade

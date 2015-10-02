a2e67f0732680a6e6f6e80fc2ecf1f73e3778240
Dylan Sadoun <didjidan@hotmail.fr>
2015-10-02 16:26:40 -0400

    Suite commit bouttons nav situationnels: jade et js

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,15 @@ router.get('/', function(req, res) {
     // Cette fonction va tenter de convertir le fichier jade situé à
     // view/index.jade en HTML pour l'envoyer vers le client.
     // On profite pour envoyer au client l'attribut « title ».
-    res.render('index', { title: "Lone Wolf" });
+    res.render('index', { title: "Lone Wolf", pageActuelle: "accueil"});
 });
 
 router.get('/nouveauPersonnage', function(req, res) {
-    res.render('nouveau_personnage');
+    res.render('nouveau_personnage', { pageActuelle: "nouveauPersonnage"});
 });
 
-
 router.get('/aide', function(req, res) {
-    res.render('aide');
+    res.render('aide', { pageActuelle: "aide"});
 });
 
 router.get('/pages/illustrations/:valeur', function(req, res) {

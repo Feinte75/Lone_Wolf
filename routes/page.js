41e13d4dea1896c34a1230c5bc18f4dfa9b35efa
Feinte75 <glenn.feunteun@telecom-bretagne.eu>
2015-10-29 22:38:12 -0400

    Ajout route JSON pour livrer les sections de pages
    Egalement ajout nombreuses pages

--- routes/page.js
+++ routes/page.js
@@ -6,23 +6,21 @@ router.get('/pagedejeu', function(req, res) {
     res.render('page_de_jeu');
 });
 
-
-router.get('/:pageNum/:section', function(req, res, next) {
+router.get('/:pageNum', function(req, res) {
   // On récupère le paramètre de l'URL
   var pageNum = req.params.pageNum
-  var section = req.params.section
   // On crée dynamiquement la page qu'on souhaite charger
-  var page = "./pages/"+ pageNum + "/page" + section + ".jade"
+  var page = "./page/"+ "page" + pageNum + ".jade"
 
   // On veut d'abord convertir la page en HTML, une fois que la conversion
   // est faite, on va injecter le HTML généré vers le fichier page.jade
   res.render(page, function(err, html) {
-      res.render('./pages/page', { title: pageNum, htmlPage: html })
+      res.render('./page/page', { title: pageNum, htmlPage: html })
   });
 });
 
 router.get('/illustrations/:valeur', function(req, res) {
-    res.sendFile('/pages/illustrations/'+req.params.valeur);
+    res.sendFile('/page/illustrations/'+req.params.valeur);
 });
 
 module.exports = router;
